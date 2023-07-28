d01f43dc4f13cf5a6493bcf2c2a025f4706b0dd5
Fernando Luppo <fernandoluppo2@gmail.com>
2023-07-28 18:31:11 -0300

    [11] test: Realizado test no arquivo usePost.ts

---
+++ src/app/shared/hook/post/usePost.test.ts
@@ -0,0 +1,25 @@
+import { usePost } from ".."
+
+describe("usePost.ts", () => {
+  it("Should return a valid value in data with isError value false", () => {
+    const value = {
+      data: {
+        value: "Valid Value"
+      }
+    }
+    const result = usePost(value)
+
+    expect(result.data).toBeDefined()
+    expect(result.isError).toBeFalsy()
+  })
+
+  it("Should return a error in data with isError value true", () => {
+    const value = {
+      response: {}
+    }
+    const result = usePost(value)
+
+    expect(result.data).toBeDefined()
+    expect(result.isError).toBeTruthy()
+  })
+})
