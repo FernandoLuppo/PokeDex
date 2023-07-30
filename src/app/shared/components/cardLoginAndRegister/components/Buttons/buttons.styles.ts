8885474dc670e97da3d9f85937c4ca235bf1a182
Fernando Luppo <fernandoluppo2@gmail.com>
2023-07-30 17:44:16 -0300

    [25] feat: Feito o estilo do botão recover password na página Login

--- src/app/shared/components/cardLoginAndRegister/components/Buttons/buttons.styles.ts
+++ src/app/shared/components/cardLoginAndRegister/components/Buttons/buttons.styles.ts
@@ -25,6 +25,23 @@ export const ContainerButtons = styled.div`
     }
   }
 
+  button:nth-child(3) {
+    cursor: pointer;
+    font-size: ${Theme.font.size.main.xs};
+    text-decoration: underline;
+    border: none;
+    color: ${Theme.font.colors.text2};
+    background: none;
+
+    @media (max-width: 365px) {
+      font-size: ${Theme.font.size.main.m};
+    }
+  }
+
+  button:nth-child(3):hover {
+    color: #a6a6a6;
+  }
+
   @media (max-width: 365px) {
     gap: 10px;
   }
