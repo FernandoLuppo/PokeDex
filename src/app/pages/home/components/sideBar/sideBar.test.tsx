219e31ef50c2fd857fecbc14e8fb3fbdf9199110
Fernando Luppo <fernandoluppo2@gmail.com>
2023-09-05 10:39:07 -0300

    [59] test: Foi testado a pagina SideBar.tsx

--- src/app/pages/home/components/sideBar/sideBar.test.tsx
+++ src/app/pages/home/components/sideBar/sideBar.test.tsx
@@ -1,24 +1,87 @@
 import { render, screen } from "@testing-library/react"
 import { SideBar } from "./SideBar"
+import { UserGetInfosContext } from "../../../../shared/context"
+
+const mockNavigate = jest.fn()
+const mockLocation = jest.fn()
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useNavigate: () => mockNavigate,
+  useLocation: () => mockLocation
+}))
 
 describe("<SideBar />", () => {
-  // Should render user photo
-
   it("should render user menu", () => {
-    render(<SideBar />)
-    const userMenu = screen.queryByRole("img", { name: "User Photo" })
+    render(
+      <UserGetInfosContext.Provider
+        value={{
+          userIsLogged: "true",
+          userInfos: { email: "test@gmail.com", name: "Test" }
+        }}
+      >
+        <SideBar />
+      </UserGetInfosContext.Provider>
+    )
+    const userMenu = screen.getByAltText("User Photo")
+    const userName = screen.getByText("Test")
+
     expect(userMenu).toBeInTheDocument()
+    expect(userName).toBeInTheDocument()
   })
 
-  it("should render filter", () => {
-    render(<SideBar />)
-    const filter = screen.getByText("Generation")
-    expect(filter).toBeInTheDocument()
+  it("Should render menu with user dislodged", () => {
+    render(
+      <UserGetInfosContext.Provider
+        value={{
+          userIsLogged: "false",
+          userInfos: { email: "", name: "" }
+        }}
+      >
+        <SideBar />
+      </UserGetInfosContext.Provider>
+    )
+
+    const loginButton = screen.getByText("Log in")
+    const signupButton = screen.getByText("Sign up")
+
+    const userMenu = screen.queryByText("User Photo")
+    const userName = screen.queryByText("Test")
+
+    expect(loginButton).toBeInTheDocument()
+    expect(signupButton).toBeInTheDocument()
+    expect(userMenu).not.toBeInTheDocument()
+    expect(userName).not.toBeInTheDocument()
   })
 
   it("should render log out button", () => {
-    render(<SideBar />)
-    const logout = screen.queryByText("Log out")
+    render(
+      <UserGetInfosContext.Provider
+        value={{
+          userIsLogged: "true",
+          userInfos: { email: "test@gmail.com", name: "Test" }
+        }}
+      >
+        <SideBar />
+      </UserGetInfosContext.Provider>
+    )
+    const logout = screen.getByText("Log out")
     expect(logout).toBeInTheDocument()
+  })
+
+  it("should not render log out button when user is dislodged", () => {
+    render(
+      <UserGetInfosContext.Provider
+        value={{
+          userIsLogged: "false",
+          userInfos: { email: "", name: "" }
+        }}
+      >
+        <SideBar />
+      </UserGetInfosContext.Provider>
+    )
+
+    const logout = screen.queryByText("Log out")
+
+    expect(logout).not.toBeInTheDocument()
   })
 })
