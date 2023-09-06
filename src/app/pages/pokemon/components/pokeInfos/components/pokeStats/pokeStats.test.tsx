35c9ac0a340bfd14500103af9877500be8719e9a
Fernando Luppo <fernandoluppo2@gmail.com>
2023-09-06 20:53:54 -0300

    [66] test: Feito os testes na página PokeStats.tsx

---
+++ src/app/pages/pokemon/components/pokeInfos/components/pokeStats/pokeStats.test.tsx
@@ -0,0 +1,30 @@
+import { render, screen } from "@testing-library/react"
+import { PokeStats } from ".."
+import type { IPokemon } from "../../../../../../shared/types"
+
+describe("<PokeStats />", () => {
+  const { pokemonStats }: IPokemon = {
+    pokemonStats: [
+      {
+        base_stat: 100,
+        name: "hp"
+      }
+    ]
+  }
+  it("Page elements should be in the document", () => {
+    render(<PokeStats pokemonStats={pokemonStats} />)
+
+    const pageTitle = screen.getByText("Base Stats")
+    expect(pageTitle).toBeInTheDocument()
+  })
+
+  it("Pokemon Stats should be in the document", () => {
+    render(<PokeStats pokemonStats={pokemonStats} />)
+
+    const stats = screen.getByText("hp:")
+    const total = screen.getByText("Total: 100")
+
+    expect(stats).toBeInTheDocument()
+    expect(total).toBeInTheDocument()
+  })
+})
